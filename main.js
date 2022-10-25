ffdabc12c75fabfb3878e25a3ed3f8c115612b3f
Minaë Tani <minae.tani@icloud.com>
2022-10-25 23:30:39 +0300

    Define JS function returning a string

---
+++ main.js
@@ -0,0 +1,13 @@
+'use Strict';
+
+const spitText = () => {
+   const textOne = 'First Sentence'; 
+   const textTwo = 'Second Sentence' ;
+   console.log(textOne);  
+   console.log(textTwo);
+  return 
+    textOne;
+    textTwo;
+}
+
+spitText();
